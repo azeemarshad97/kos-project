8a24cf008e5a947f97d426f6a963315c87dd4c89
fabrice <fab.hatege15@gmail.com>
2022-05-18 09:32:33 +0200

    SPARQL query test with queries.py

--- interface/queries.py
+++ interface/queries.py
@@ -1,13 +1,15 @@
 from owlready2 import *
 
-path = "ontology_v2.owl"
+# path = "ontology_v2.owl"
+path = "ontology_and_index.owl"
 
 onto = get_ontology("file://"+path).load()
 
 query = """
            SELECT ?x
-           { ?x a owl:Class . }
+           { ?x a ontology_and_index:_Localisation . }
     """
 
 # SPARQL Query
 print(list(default_world.sparql(query)))
+# { ?x a ontology_and_index:_Localisation . }
