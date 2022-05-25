a532f44d6075a38e69217162c4343f30257d3734
fabrice <fab.hatege15@gmail.com>
2022-05-25 13:49:32 +0200

    analyze of the sparql queries

--- interface/queries.py
+++ interface/queries.py
@@ -6,10 +6,20 @@ path = "ontology_and_index.owl"
 onto = get_ontology("file://"+path).load()
 
 query = """
-           SELECT ?x
-           { ?x a ontology_and_index:_Localisation . }
+           SELECT ?y WHERE
+           { 
+               ?x a ontology_and_index:_Localisation.
+               ?x ?y ?z.
+           }
     """
+
+# LINK CODE
+"""
+is : 6
+"""
 
 # SPARQL Query
 print(list(default_world.sparql(query)))
-# { ?x a ontology_and_index:_Localisation . }
+# print(len(list(default_world.sparql(query))[1]))
+# print(list(default_world.sparql(query))[1][2].name)
+
