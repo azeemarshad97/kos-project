6bf32c16d596a44f6e03ee96b04baa7b4ae50b88
fabrice <fab.hatege15@gmail.com>
2022-05-25 13:59:04 +0200

    ajout d'une fonction de formatage

--- interface/queries.py
+++ interface/queries.py
@@ -6,7 +6,7 @@ path = "ontology_and_index.owl"
 onto = get_ontology("file://"+path).load()
 
 query = """
-           SELECT ?y WHERE
+           SELECT ?x ?y ?z WHERE
            { 
                ?x a ontology_and_index:_Localisation.
                ?x ?y ?z.
@@ -18,8 +18,25 @@ query = """
 is : 6
 """
 
+# Fonction de formatage d'element
+def formatElement(element):
+    if element == 6:
+        return "a"
+    else:
+        return element.name
+
+# Fonction de conversion
+def formatResults(table):
+    nouvelle_table = []
+    for ligne in table:
+        nouvelle_ligne = []
+        for element in ligne:
+            nouvelle_ligne.append(formatElement(element))
+        nouvelle_table.append(nouvelle_ligne)
+    return nouvelle_table
+
 # SPARQL Query
-print(list(default_world.sparql(query)))
+print(formatResults(list(default_world.sparql(query))))
 # print(len(list(default_world.sparql(query))[1]))
 # print(list(default_world.sparql(query))[1][2].name)
 
